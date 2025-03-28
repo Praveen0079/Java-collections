989d5db7f498726fe61fdfc0b52624c46e2edd56
Praveen Sharma <piyush20112@gmail.com>
2025-03-28 22:00:23 +0530

    [Praveen] ADD: Sorted Set

---
+++ src/main/java/com/bridgelabz/Set_Interface/SortedSet.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.Set_Interface;
+
+import java.util.*;
+
+public class SortedSet {
+    // Main method
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        set.add(5);
+        set.add(3);
+        set.add(9);
+        set.add(1);
+
+        List<Integer> sortedList = convertToSortedList(set);
+        System.out.println("Sorted List: " + sortedList);
+    }
+
+    public static List<Integer> convertToSortedList(Set<Integer> set){
+        List<Integer> sortedList = new ArrayList<>(set);
+
+        // Sort the list in ascending order
+        Collections.sort(sortedList);
+
+        return sortedList;
+    }
+}
